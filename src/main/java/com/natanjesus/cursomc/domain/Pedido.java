7e6ab07028ac1d7b389b5f67c3afd904fa483980
Natan Jesus <natan.jesus@kaspper.com>
2020-08-18 22:22:01 -0300

    SubTotal e Total do Pedido feitos dentro do objeto

--- src/main/java/com/natanjesus/cursomc/domain/Pedido.java
+++ src/main/java/com/natanjesus/cursomc/domain/Pedido.java
@@ -103,6 +103,14 @@ public class Pedido implements Serializable {
 		this.itens = itens;
 	}
 
+	public Double getValorTotal(){
+		double soma = 0.0;
+		for (ItemPedido item : this.itens) {
+			soma += item.getSubTotal();
+		}
+		return soma;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
